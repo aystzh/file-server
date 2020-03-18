70d10c9afab7fe3795b1f04cc712a948736b4ab7
zhanghuan <aystzh@163.com>
2020-03-19 01:20:33 +0800

    端口修改

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func main()  {
-	http.Handle("/",http.FileServer(http.Dir(".")))
-	http.ListenAndServe(":8080", nil)
+func main() {
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":8081", nil)
 }
